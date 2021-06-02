bd716ae14f3aabcb2f530b54b6f0302483c07897
Sascha Majewsky <sascha.majewsky@pm.me>
2021-06-02 02:51:23 +0200

    Added get recent prunes logic

--- routes/url-router.js
+++ routes/url-router.js
@@ -11,7 +11,7 @@ router.get("/", async (req, res) => {
     res.status(200).json({ urls: urls });
   } catch (err) {
     console.log("Error during listing all urls: ", err);
-    res.status(500).json({ message: "Error during listing all urls" });
+    return res.status(500).json({ message: "Error during listing all urls" });
   }
 });
 
@@ -22,14 +22,17 @@ router.get("/:id", async (req, res) => {
       params: { id },
     } = req;
 
-    const url = await urlServices.findByUrlId(id);
+    const url = await urlServices.findByMappedUrl(id);
     res.status(200).json({ url: url });
   } catch (err) {
     if (err.toString().includes("Cast to ObjectId failed for value")) {
+      console.log("Error not found: ", err);
       res.status(404).json({ message: `Url was not found.` });
     } else {
       console.log("Error during listing a specific url: ", err);
-      res.status(500).json({ message: "Error during listing a specific url" });
+      return res
+        .status(500)
+        .json({ message: "Error during listing a specific url" });
     }
   }
 });
@@ -45,14 +48,19 @@ router.post("/add", async (req, res) => {
         return res.status(200).json({ url: foundUrl });
       }
 
-      const newUrl = await urlServices.createUrl(parsedUrl);
+      const newUrl = await urlServices.createUrl(
+        parsedUrl,
+        req.body.recentUserId
+      );
       res.status(201).json({ url: newUrl });
     } else {
       res.status(400).json({ message: "Provided url is malformed." });
     }
   } catch (err) {
     console.log("Error during adding a specific url: ", err);
-    res.status(500).json({ message: "Error during adding a specific url" });
+    return res
+      .status(500)
+      .json({ message: "Error during adding a specific url" });
   }
 });
 
@@ -72,7 +80,7 @@ router.get("/viewed/:code", async (req, res) => {
     res.status(200).json({ updatedUrl });
   } catch (err) {
     console.log("Error during adding a view to a specific url: ", err);
-    res
+    return res
       .status(500)
       .json({ message: "Error during adding a view to a specific url" });
   }
@@ -87,17 +95,39 @@ router.get("/redirect/:code", async (req, res) => {
 
     const foundUrl = await urlServices.findByMappedUrl(code);
     if (!foundUrl) {
+      console.log("Error mapped url not found: ", recentUserId);
       return res
         .status(404)
         .json({ message: `Url mapped to ${code} was not found.` });
     }
     await urlServices.updateUrlView(code);
-    res.redirect(foundUrl.url);
+    return res.redirect(foundUrl.url);
   } catch (err) {
     console.log("Error during redirecting to a specific url: ", err);
-    res
+    return res
       .status(500)
       .json({ message: "Error during redirecting to a specific url" });
+  }
+});
+
+// GET last three prunes for a recentUserId
+router.post("/history", async (req, res) => {
+  try {
+    const { recentUserId } = req.body;
+
+    const foundPrunes = await urlServices.getMostRecentPrunes(recentUserId);
+    if (!foundPrunes) {
+      console.log("Error no recent mapped urls found: ", recentUserId);
+      return res
+        .status(404)
+        .json({ message: `No recent prunes found for ${recentUserId}.` });
+    }
+    return res.status(200).json({ foundPrunes });
+  } catch (err) {
+    console.log("Error during retrieving recent prunes: ", err);
+    return res
+      .status(500)
+      .json({ message: "Error during retrieving recent prunes" });
   }
 });
 
